0e6e35820c4e2bed3475afef6960fa8c8f78e494
0xVolta <Ondrejrehacek@gmail.com>
2023-05-08 11:45:15 +0200

    Fixed signup bug, I,proved onramp script.

---
+++ Assets/Scripts/FractalSDK/Core/FractalOnrampHandler.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using FractalSDK.Models.Api;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FractalSDK.Core
+{
+    public class FractalOnrampHandler : MonoBehaviour
+    {
+        #region Fields
+
+        [Tooltip("Button object to manually initiate the onramp process.")]
+        public Button onrampButton;
+
+        [Tooltip("Game clientId you can obtain from Fstudio.")]
+        public string clientId;
+
+        #endregion
+
+        #region Externals
+
+        //[External JS Call]
+        [DllImport("__Internal")]
+        private static extern void OpenFractalPopup(string url);
+
+        #endregion
+
+
+        void Start()
+        {
+            Button onrampPrefab = onrampButton.GetComponent<Button>();
+            onrampPrefab.onClick.AddListener(OpenOnramp);
+        }
+
+
+        /// <summary>
+        /// Opens Fractal authentication URL in the systems default browser.
+        /// On WebGL a Fractal plugin is used to open the authentication in popup.
+        /// </summary>
+        /// <param name="authUrl">Authentication URL to open.</param>
+        private void OpenOnramp()
+        {
+            string baseUrl= "https://www.fractal.is/onramp?clientId=";
+
+            switch (Application.platform)
+            {
+                case RuntimePlatform.WebGLPlayer:
+                    OpenFractalPopup(baseUrl + clientId);
+                    break;
+
+                default:
+                    Application.OpenURL(baseUrl + clientId);
+                    break;
+            }
+
+        }
+
+    }
+}
